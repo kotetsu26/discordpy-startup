527599ae44be714e42ff217c36716520048aa904
kotetsu26 <45928000+kotetsu26@users.noreply.github.com>
2019-08-01 00:33:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,6 +18,12 @@ async def ping(ctx):
 @bot.command()
 async def neko(ctx):
     await ctx.send('にゃーん')
+    
+@client.event
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+        reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+        await message.channel.send(reply) # 返信メッセージを送信
 
 
 bot.run(token)
