fb74c5d4b2acbdae1b4d04ca147d75b156713c96
kotetsu26 <45928000+kotetsu26@users.noreply.github.com>
2019-08-01 01:25:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,16 +18,23 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def ||rule||(ctx):
-    await ctx.send('||女勇者||')
-    await ctx.send('||村人(赤)||')
+async def rule(ctx):
+    await ctx.send('プレイヤー１は「女勇者」か「村人（赤）」「村人（緑）」「村人（青）」を出せます。')
+    await ctx.send('プレイヤー２は「魔王」か「スライム（赤）」「スライム（緑）」「スライム（青）」を出せます。')
+    await ctx.send('「女勇者」は「魔王」に勝てますが、いずれの「スライム」にも勝つことはできません。')
+    await ctx.send('「村人」は、自分と同じ色の「スライム」にのみ勝つことができます')
     
 @bot.command()
-async def on_message(message):
-    await ctx.send('ユルサナイ')
-    if client.user in message.mentions: # 話しかけられたかの判定
-        reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
-        await message.channel.send(reply) # 返信メッセージを送信
+async def reset(ctx):
+    Player1 = None
+    Player2 = None
+    await ctx.send('リセットしました')
+    
+@bot.command()
+async def 女勇者(ctx):
+    Player1 = "女勇者"
+    await message.channel.send(Player1)
+    await ctx.send('リセットしました')
 
 
 bot.run(token)
