565fa91c7faf5edbfb335f692ac1a009daf3fedd
kotetsu26 <45928000+kotetsu26@users.noreply.github.com>
2019-08-01 00:50:22 +0900

    Test6

--- discordbot.py
+++ discordbot.py
@@ -16,10 +16,15 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def neko(ctx):
+async def rule(ctx):
     await ctx.send('||女勇者||')
     await ctx.send('||村人(赤)||')
     
+@bot.command()
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+        reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+        await message.channel.send(reply) # 返信メッセージを送信
 
 
 bot.run(token)
